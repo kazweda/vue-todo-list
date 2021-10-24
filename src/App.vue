d7be327c495e8b918e29478d05f053925b4e15e9
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2021-10-24 13:47:30 +0900

    fix #5

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
+  <img :src="logoURL" :alt="logoCaption" width="200" height="200" />
   <h1>{{ title }}</h1>
 
   <h2>Add a new task</h2>
+  <span>You have {{ allTasks }} {{ allTasks > 1 ? 'tasks' : 'task' }} at the moment</span>
 
   <div>
     <input type="text"
       v-model="newTask"
+      @keyup.enter="addTask"
       placeholder="Add a new task"
     >
     <button
@@ -21,19 +24,20 @@
     <p>{{ newTask }}</p>
   </div>
 
-  <ul v-for="task in tasks" :key="task.id">
-    <li v-if="task.id <= maxTasks">
-        {{task.id}}. {{ task.name }}
+  <ul
+    v-for="(task, index) in latest" 
+    :key="task.id"
+    @click="finishTask(task)"
+    :class="{ strikeout: task.finished }"
+  >
+    <li
+      v-if="index < maxTasks"
+    >
+      {{ index + 1 }}. {{ task.name }}
 
-        <div v-if="task.finished">
-          <button>Delete task</button>
-        </div>
-        <div v-else-if="task.edit">
-          <button>Edit task</button>
-        </div>
-        <div v-else>
-          <p>No button</p>
-        </div>
+      <div v-if="task.finished">
+          <button @click="removeTask(task.id)">Delete task</button>
+      </div>
     </li>
   </ul>
 </template>
@@ -44,12 +48,14 @@ export default {
     return {
       title: 'My To Do App',
       newTask: '',
+      logoURL: 'https://images.unsplash.com/photo-1507925921958-8a62f3d1a50d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1955&q=80',
+      logoCaption: 'A photo by Kelly Sikkema on Unsplash showing post-it notes',
+      maxTasks: 5,
       tasks: [
         { id: 1, name: 'Learn Vue JS', finished: false },
         { id: 2, name: 'Build a Vue application', finished: true },
         { id: 3, name: 'Write an article about Vue JS', finished: false }
-      ],
-      maxTasks: 5
+      ]
     }
   },
   methods: {
@@ -63,7 +69,29 @@ export default {
       });
 
       this.newTask = ''
+    },
+    removeTask(taskID) {
+      this.tasks = this.tasks.filter(task => {
+          return task.id !== taskID
+      });
+    },
+    finishTask(task) {
+      task.finished = !task.finished
+    }
+  },
+  computed: {
+    allTasks() {
+        return this.tasks.length
+    },
+    latest() {
+        return [...this.tasks].reverse()
     }
   }
 }
 </script>
+
+<style>
+.strikeout {
+  text-decoration: line-through;
+}
+</style>
